3b6bd79a8dee1a9ced2b8a94ff292a92720509cf
Dan Rumney <dan.rumney@ohanapediatrics.com>
2018-08-21 13:23:16 -0500

    Export shape constants

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import VolumeMeter from './components/VolumeMeter';
+import VolumeMeter, { VM_FLAT, VM_STEPPED } from './components/VolumeMeter';
 
 export default VolumeMeter;
+export { VM_FLAT };
+export { VM_STEPPED };
